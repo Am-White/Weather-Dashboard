c999fe470e5d21e1099392e5b0835d6b6d6cd543
littlefire <aybwhite@gmail.com>
2020-12-02 01:13:48 -0800

    connecting css

---
+++ assetss/js/script.js
@@ -0,0 +1,14 @@
+// API key
+const apiKey ="d0c7a236e73b7d700b29c6edb3005c4a";
+//Variables
+const searchBtn = $('#search-btn');
+let inputCity = $('#input-search');
+let cities = $('.city');
+let resultCitEL = $('#current-location');
+let resultTempEL = $('#current-temp');
+let resultWindEL = $('#current-wind');
+let resultHumEl = $('#current-hum');
+let resultUvEL =$('#current-uv');
+let uvColor = $('.uvColor');
+let fahreneitEl = $('#fahreneit');
+
